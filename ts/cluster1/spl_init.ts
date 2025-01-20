41b16ceed2323cb5288b8ba21d3f8d0bdc4f2afd
baindlapranayraj <pranayraj069@gmail.com>
2025-01-21 04:24:46 +0530

    day 1 done

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { createMint } from "@solana/spl-token";
+import wallet from "../wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,10 +10,23 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    // Start here
+    console.log("Creating your min-account.....");
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      keypair.publicKey,
+      6
+    );
+    console.log(`The mint account pubkey is: ${mint.toString()}`);
+
+    let mintAccount = await connection.getAccountInfo(mint, "confirmed");
+    // console.log("The mint accountInfo", JSON.stringify(mintAccount));
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
+
+// Hear we are creating the Mint Account.
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,29 +1,36 @@
-import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { Keypair, PublicKey, Connection, Commitment, clusterApiUrl } from "@solana/web3.js";
+import {  getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import wallet from "../wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const connection = new Connection(clusterApiUrl("devnet"), commitment);
 
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("37ABqiPxq5mQWLUKYN7oaHkv2qdZtq6DHiwybwSBbwej");
 
 (async () => {
     try {
         // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+        const ata = await getOrCreateAssociatedTokenAccount(connection,keypair,mint,keypair.publicKey);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+        console.log(" ");
+        // let accountInfo = await connection.getAccountInfo(ata.address);
+        // console.log("accountInfo: ",accountInfo);
+    
 
         // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
+        const mintTx = await mintTo(connection,keypair,mint,ata.address,keypair,token_decimals);
+
+        console.log(`Your mint txid: ${mintTx}`);
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
+
+
